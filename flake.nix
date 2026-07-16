0c34ae67388b5589a798f903ad64dd344d5564a7
Gutyina Gergő <gutyina.gergo.2@gmail.com>
2026-07-17 00:16:50 +0200

    chore: clean up flake.nix

--- flake.nix
+++ flake.nix
@@ -3,25 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # flake-parts
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    flake-root.url = "github:srid/flake-root";
-    # pre-commit-hooks-nix = {
-    #   url = "github:hercules-ci/pre-commit-hooks.nix/flakeModule";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
-    # utils
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    statix = {
-      url = "github:nerdypepper/statix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -35,7 +22,6 @@
     flake-parts.lib.mkFlake {inherit inputs;}
     {
       imports = [
-        inputs.flake-root.flakeModule
         inputs.treefmt-nix.flakeModule
       ];
       systems = ["x86_64-linux"];
@@ -55,7 +41,6 @@
           ];
         };
         treefmt.config = {
-          inherit (config.flake-root) projectRootFile;
           package = pkgs.treefmt;
 
           programs = {
